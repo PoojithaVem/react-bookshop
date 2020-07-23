6529c5ae139e0caa7be40ff9ec6d172128c34ed7
poojitha.vr <pooji96@gmail.com>
2020-07-23 21:17:46 +0530

    Updates

---
+++ precache-manifest.2ce38f7544344163d3d382f3e004c6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499528de006b7c1ca7609b1d00a36111",
+    "url": "/react-bookshop/index.html"
+  },
+  {
+    "revision": "0446c8426ae5f5116718",
+    "url": "/react-bookshop/static/css/main.6b70546d.chunk.css"
+  },
+  {
+    "revision": "daa3c69a36171a513939",
+    "url": "/react-bookshop/static/js/2.5d3eb82d.chunk.js"
+  },
+  {
+    "revision": "b2490fbb34248c3403116f401942d851",
+    "url": "/react-bookshop/static/js/2.5d3eb82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0446c8426ae5f5116718",
+    "url": "/react-bookshop/static/js/main.7dc7236a.chunk.js"
+  },
+  {
+    "revision": "f6e313a9b0b95ff7e55f",
+    "url": "/react-bookshop/static/js/runtime-main.ebefbc74.js"
+  }
+]);
